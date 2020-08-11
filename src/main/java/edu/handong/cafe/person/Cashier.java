49990ab1746bb10cef9951a72cb54cb36418ea33
kongsubin <qls0919@naver.com>
2020-08-11 11:30:05 +0900

    add method and variable in Cashie class

--- src/main/java/edu/handong/cafe/person/Cashier.java
+++ src/main/java/edu/handong/cafe/person/Cashier.java
@@ -1,5 +1,29 @@
 package edu.handong.cafe.person;
 
 public class Cashier extends Human {
+	
+	public Cashier(String name, int age) {
+		super(name, age);
+	}
+	
+	public Cashier() {
+		super("unName", 0);
+	}
+	
+	public void takeOrder() {
+		System.out.println("Cashier take order from customer. ");
+		System.out.println("Cashier : Do you want to oder?? ");
+	}
+	public void recieveMoney() {
+		System.out.println("Cashier recieve money from customer. ");
+		System.out.println("Cashier : Thank you for your odering!");
+	}
+	public void giveFinishSignal() {
+		// Vibration bell or Oder Number paper
+		System.out.println("Cashier give the customer the ability to confirm that their order has been served.");
+		System.out.println("Here you are!");
+	}
+	
 
 }
+
