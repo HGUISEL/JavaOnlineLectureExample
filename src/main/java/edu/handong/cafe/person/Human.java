4e07bf80e509a15ffc7d2aa96fa64767af0a9237
kongsubin <qls0919@naver.com>
2020-08-11 11:29:15 +0900

    add method and variable in Cafe class

--- src/main/java/edu/handong/cafe/person/Human.java
+++ src/main/java/edu/handong/cafe/person/Human.java
@@ -1,5 +1,31 @@
 package edu.handong.cafe.person;
 
 public class Human {
-
+	protected String name;
+	protected int age;
+	
+	public Human(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	
+	public Human() {
+		this.name = "unname";
+		this.age = 0;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
 }
