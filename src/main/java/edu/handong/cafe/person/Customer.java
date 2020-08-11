45eb6a6343ac137c5c560f62b09f3c442d773a6b
kongsubin <qls0919@naver.com>
2020-08-11 11:29:45 +0900

    add method and variable in Customer class

--- src/main/java/edu/handong/cafe/person/Customer.java
+++ src/main/java/edu/handong/cafe/person/Customer.java
@@ -1,5 +1,29 @@
 package edu.handong.cafe.person;
+import java.util.Scanner;
 
 public class Customer extends Human {
-
+	
+	public Customer(String name, int age) {
+		super(name, age);
+	}
+	
+	public Customer() {
+		super("unName", 0);
+	}
+	
+	public int orderMenu() {
+		Scanner keyboard = new Scanner(System.in);
+		int menuNum = 0;
+		
+		System.out.println("Please Input the Menu Number");
+		System.out.print("Customer : Can I get a ");
+		menuNum = keyboard.nextInt();
+		
+		return menuNum;
+	}
+	
+	public void payMoney() {
+		System.out.println("Customer pays the money!");
+	}
+	
 }
