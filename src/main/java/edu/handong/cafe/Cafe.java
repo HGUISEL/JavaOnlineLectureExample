e37b8d262b77797bf22a76888b38f7c850e76bc0
kongsubin <qls0919@naver.com>
2020-08-11 11:30:39 +0900

    add method and variable in Cafe class

--- src/main/java/edu/handong/cafe/Cafe.java
+++ src/main/java/edu/handong/cafe/Cafe.java
@@ -1,5 +1,27 @@
 package edu.handong.cafe;
+import edu.handong.cafe.person.Barister;
+import edu.handong.cafe.person.Cashier;
+
 
 public class Cafe {
+	protected Barister barista;
+	protected Cashier cashier;
+	protected int numberOfSits;
+	
+	public void showMenu() {
+		
+	}
+	public void takeOrder() {
+		
+	}
+	public void recieveMoney() {
+		
+	}
+	public void makeDrink() {
+		
+	}
+	public void makeBread() {
+		
+	}
 
 }
