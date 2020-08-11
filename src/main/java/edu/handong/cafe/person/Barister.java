3422f8cbbc69d0c55174ac95fe935600e42d5a9e
kongsubin <qls0919@naver.com>
2020-08-11 11:28:55 +0900

    add method and variable in Barister class

--- src/main/java/edu/handong/cafe/person/Barister.java
+++ src/main/java/edu/handong/cafe/person/Barister.java
@@ -2,4 +2,26 @@ package edu.handong.cafe.person;
 
 public class Barister extends Human {
 
+	public Barister(String name, int age) {
+		super(name, age);
+	}
+	
+	public Barister() {
+		super("unName", 0);
+	}
+	
+	public void makeDrink(String menu) {
+		System.out.println("Barister is making the " + menu);
+		try {
+		      Thread.sleep(5000);
+		} catch (InterruptedException e) { 
+			
+		}		
+		System.out.println("Barister : done!!! ");	
+	}
+
+	public void cleanCafe() {
+		System.out.println("Barister is cleaning the Cafe");
+	}
+
 }
