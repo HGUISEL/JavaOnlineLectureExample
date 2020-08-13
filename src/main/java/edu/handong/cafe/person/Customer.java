83a52c4f889586bddf087579744cbe4b460fa168
kongsubin <qls0919@naver.com>
2020-08-13 10:49:26 +0900

    fix customer file

--- src/main/java/edu/handong/cafe/person/Customer.java
+++ src/main/java/edu/handong/cafe/person/Customer.java
@@ -2,17 +2,48 @@ package edu.handong.cafe.person;
 import java.util.Scanner;
 
 public class Customer extends Human {
-	
-	public Customer(String name, int age) {
-		super(name, age);
-	}
+	Scanner keyboard;
 	
 	public Customer() {
-		super("unName", 0);
+		this.keyboard = new Scanner(System.in);
+		this.setName();
+		this.setAge();
+	}
+	
+	public void setName() {
+		System.out.print("Enter Customer's Name :  ");
+		String name = keyboard.next();
+		super.setName(name);
+		
+	}
+
+	public void setAge() {
+		System.out.print("Enter Customer's age :  ");
+		int age;
+		
+		try {
+			age = keyboard.nextInt();
+		}catch(Exception e) {
+			System.out.println("Wrong Input");
+			age = 0;
+		}
+		super.setAge(age);
+	}
+	
+	public int chooseCafe() {
+		System.out.print("Enter Cafe Name that you want go :  ");
+		int cafeType;
+		
+		try {
+			cafeType = keyboard.nextInt();
+			return cafeType;
+		}catch(Exception e) {
+			System.out.println("Wrong Input");
+			return 0;
+		}	
 	}
 	
 	public int orderMenu() {
-		Scanner keyboard = new Scanner(System.in);
 		int menuNum = 0;
 		
 		System.out.println("Please Input the Menu Number");
--- src/main/java/edu/handong/cafe/person/Human.java
+++ src/main/java/edu/handong/cafe/person/Human.java
@@ -8,15 +8,23 @@ public class Human {
 		this.name = name;
 		this.age = age;
 	}
+	
+	public Human() {
+		
+	}
+	
 	public String getName() {
 		return name;
 	}
+	
 	public void setName(String name) {
 		this.name = name;
 	}
+	
 	public int getAge() {
 		return age;
 	}
+	
 	public void setAge(int age) {
 		this.age = age;
 	}
