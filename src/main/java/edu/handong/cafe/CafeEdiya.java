9302c1ca4a615fdd16c42236ed01c9015deaa259
JC <jc@lifove.net>
2020-08-19 11:50:15 +0900

    Minor change

--- src/main/java/edu/handong/cafe/CafeEdiya.java
+++ src/main/java/edu/handong/cafe/CafeEdiya.java
@@ -3,12 +3,14 @@ package edu.handong.cafe;
 import edu.handong.cafe.person.Barister;
 
 public class CafeEdiya extends Cafe {
-	private Barister barista;
+	private Barister secondBarista;
 	
 	public CafeEdiya() {
 		System.out.println("\nWelcome to Ediya!!!");
 		barista.setName("Ediya Staff");
 		barista.setAge(25);
+		secondBarista.setName("Ediya Assistant");
+		secondBarista.setAge(23);
 	}
 	
 	public void givePoint() {
